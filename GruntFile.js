e8486ba7d69745dcf11eb757be0d115b84079843
Steve Vadocz <steve.vadocz@guardian.co.uk>
2015-04-12 19:21:45 +0100

    Inital setup files

---
+++ GruntFile.js
@@ -0,0 +1,133 @@
+/**
+ * GruntFile
+ * @version 1.0.0
+ */
+module.exports = function(grunt) {
+
+    // load all grunt tasks
+    require('matchdep').filterDev(['grunt-*', '!grunt-cli']).forEach(grunt.loadNpmTasks);
+
+    grunt.initConfig({
+
+        /**
+        * Read package.json
+        */
+        pkg: grunt.file.readJSON('package.json'),
+
+        dir: {
+            js: 'public/js',
+            css: 'public/css',
+            sass: 'assets/sass',
+            img: 'public/images',
+            views: 'public/views'
+        },
+
+        /**
+         * Compile SASS
+         */
+        sass: {
+            dev: {
+                options: {
+                    style: 'expanded',
+                    trace: true,
+                    debugInfo: true
+                },
+                expand: true,
+                cwd: '<%= dir.sass %>/',
+                src: ['*.scss', '_*.scss'],
+                dest: '<%= dir.css %>',
+                ext: '.css'
+            },
+            build: {
+                options: {
+                    style: 'compressed',
+                    noCache: true
+                },
+                expand: true,
+                cwd: '<%= dir.sass %>/',
+                src: ['*.scss', '!_*.scss'],
+                dest: '<%= dir.css %>',
+                ext: '.min.css'
+            }
+        },
+
+        autoprefixer: {
+            dev: {
+                options: {
+                    browsers: ['last 2 versions', 'ie 8', 'ie 9']
+                },
+                expand: true,
+                flatten: true,
+                src: '<%= dir.css %>/*.css',
+                dest: '<%= dir.css %>/'
+            }
+        },
+
+        // The watch command watches a given set of files and runs a task when one of them changes.
+        watch: {
+
+            //Automatic compilation of SASS changes
+            sass: {
+                files: ['<%= dir.sass %>/**/*.scss'],
+                tasks: ['sass:dev', 'notify:sass']
+            },
+
+            // Add vendor prefixes
+            prefix: {
+                files: ['*.scss'],
+                tasks: ['autoprefixer:dev', 'notify:prefix'],
+                options: {
+                    livereload: true
+                }
+            },
+        },
+
+        // In order to run the Karma watcher and the SASS watchers concurrently, we need to run this task
+        concurrent: {
+            dev: {
+                tasks: ['watch'],
+                options: {
+                    logConcurrentOutput: true
+                }
+            }
+        },
+
+        notify: {
+            dev: {
+                options: {
+                    message: "Dev changes complete."
+                }
+            },
+
+            build: {
+                options: {
+                    message: "Build complete."
+                }
+            },
+
+            sass: {
+                options: {
+                    message: "SASS compiled."
+                }
+            },
+
+            prefix: {
+                options: {
+                    message: "Autoprefixer finished."
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('dev', [
+        'concurrent:dev',
+        'notify:dev'
+    ]);
+
+    grunt.registerTask('build', [
+        'sass:build',
+        'notify:build'
+    ]);
+
+    grunt.registerTask('prefix', ['autoprefixer:dev']);
+};
